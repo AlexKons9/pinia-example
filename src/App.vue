7caf86000778c53c0fed9bbad68fb7de86706515
AlexKons9 <alex.lepeniotis@gmail.com>
2023-05-29 10:11:05 +0300

    Again some changes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
-    
+
+    <!-- new task form -->
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
     <!-- filter -->
     <nav class="filter">
       <button @click="filter = 'all'">All tasks</button>
@@ -14,13 +19,13 @@
 
     <!-- task list -->
     <div class="task-list" v-if="filter === 'all'">
-      <p>all tasks</p>
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
       <div v-for="task in taskStore.tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>fav tasks</p>
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
       <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
@@ -36,12 +41,14 @@ import { ref } from 'vue'
 import './assets/main.css'
 import TaskDetails from './components/TaskDetails.vue'
 import { useTaskStore } from './stores/TaskStore'
+import TaskForm from './components/TaskForm.vue'
 
 export default {
 
   components: {
-    TaskDetails
-  },
+    TaskDetails,
+    TaskForm
+},
 
   setup() {
     const taskStore = useTaskStore();
